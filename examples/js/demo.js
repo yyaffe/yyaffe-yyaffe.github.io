45dfe19dbbb4dd5644e70c52e9817b72e485e849
yyaffe <yoel.yaffe@cyberark.com>
2020-04-21 15:54:12 +0300

    action fix

--- examples/js/demo.js
+++ examples/js/demo.js
@@ -22,6 +22,9 @@ var conditions, actions, nameField, ageField, occupationField, submit, allData;
               "options": []}
                         ],
           "actions": [
+             { "name": "put_on_sale",
+              "label": "Put On Sale",
+              "params": {"sale_percentage": "numeric"}},
             { "name": "get_rule_resulting_information",
               "label": "get_rule_resulting_information",
               "params": [{"rule_id":"numeric"}, {"secret_id":"numeric"}, {"priority":"numeric"}]}
